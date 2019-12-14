29ee0481e00db64989b1ec78a2e8acf762adff4f
Patience Condell <condellp@gmail.com>
2019-12-13 19:54:42 -0500

    Learning my homework

---
+++ liri.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+var keys = require("./keys");
+
+var moment = require("moment");
+moment().format()
+
+var Spotify = require('node-spotify-api');
+
+var axios = require("axios")
+
+var fs = require("fs");
+
