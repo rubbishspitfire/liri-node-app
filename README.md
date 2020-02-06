980ba12940df46bf2d55bfccaa5bfd2e8dab4ec4
Patience Condell <condellp@gmail.com>
2020-02-05 19:09:47 -0500

    adding changes & deleting files

--- README.md
+++ README.md
@@ -1,3 +1,71 @@
-# liri-node-app
+# LIRI - Language Interpretation and Recognition Interface
 
-Welcome to the Liri-Node-App By Patience Condell
+## Overview
+LIRI is a command line `node js` app that takes command line parameters and returns the data retrieved from different web API's.      Here are LIRI-commands: 
+* `concert-this` 
+* `spotify-this-song`
+* `movie-this`
+* `do-what-it-says`
+
+Result will be displayed on the terminal and will also be appended to `log.txt`
+
+## What each command does:
+(1) `concert-this <artist/band name here>`:
+This command will search the [_Bands in Town Artist Events_](https://artists.bandsintown.com/support/bandsintown-api) API for the artist/band name and returns the following information about each event to the terminal: 
+* Name of the venue,
+* Venue location,
+* Date of the Event.
+
+![](./images/concertthis.PNG)
+
+(2) `spotify-this-song <song name here>`: This command uses [_Spotify API_](https://developer.spotify.com/documentation/web-api/) to fetch details like, 
+* Song name
+* Song artist,
+* Preview link of song from Spotify,
+* Album name.
+
+If no song name is provided, then it defaults to "The Sign" by Ace of Base.
+
+![](./images/spotifythissong.PNG)
+
+(3) `movie-this <movie name here>`: This command makes use of [_OMDB_](http://www.omdbapi.com/) API to search movie details like, 
+* Release year,
+* Country,
+* Language,
+* IMDB rating, 
+* Rotten Tomatoes rating,
+* Movie plot, 
+* Actors. 
+
+If no movie name is provided, then it defaults to "Mr. Nobody" movie.
+
+![](./images/moviethis.PNG)
+
+(4) `do-what-it-says`: LIRI will execute command from random.txt file.Current command in _random.txt_ is `spotify-this-song,"I want it that way"`
+![](./images/dowhatitsays.PNG)
+
+## Prerequisite
+`Node.js - Download the latest version of Node https://nodejs.org/en/`
+
+## Getting Started
+* Clone this repository.
+* On your CLI terminal execute command `npm install` to install the dependancies.
+* Set following environmental variables
+* APP_ID = _your Bands in town artist event API key_
+* SPOTIFY_ID = _your spotify api id_
+* SPOTIFY_SECRET = _your spotify api secret key_
+
+To execute the script use syntax:
+`node liri.js <LIRI-command> <Artist Name/Band Name/Movie Name/Track Name>`
+
+## Technologies Used
+* NodeJS
+* JavaScript
+* Bandsintown API
+* Spotify API
+* OMDB API
+* NPM fs
+* NPM dotenv
+* NPM request
+* Moment.js
+* NPM axios
--- liri.js
+++ liri.js
@@ -1,13 +1,102 @@
 require("dotenv").config();
-
 var keys = require("./keys");
-
-var moment = require("moment");
-moment().format()
-
+var inquirer = require('inquirer');
 var Spotify = require('node-spotify-api');
-
-var axios = require("axios")
-
+var Twitter = require('twitter');
 var fs = require("fs");
+var request = require("request");
 
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+inquirer.prompt([
+    {
+        type: "list",
+        name: "userChoice",
+        message: "Hello Patience, this is Liri. What can I help you with?",
+        choices: ["Seach Twitter", "Get Spotify",  "Search movie", "Call mom"]
+    },
+    {
+        type: "input",
+        name: "searchQuery",
+        message: "What can I help you?"
+    },
+
+]).then(function(response) {
+    var response = response;
+    if (response.userChoice === "Movie info") {
+        var queryUrl = ("http://www.omdbapi.com/?t=underworld&y=&plot=short&apikey=trilogy").then(
+            function(response) {
+                console.log("The movie's rating is: " + response.data.imdbRating);
+        })
+        .catch(function(error) {
+            if (error.response) {
+     console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+        console.log(error.request);
+    } else {
+        console.log("Error", error.message);
+    }
+    console.log(error.config);
+           
+    });
+       
+        request(queryUrl, function (error, repsonse, body) {
+            if (!error && response.statusCode === 200) {
+                var data = JSON.parse(body);
+                var ratingText = '';
+                var ratings = data.Ratings.filter(function (rating) {
+                    var notMetacritic = rating.Source !== "Metacritic";
+                    if (notMetacritic) {
+                        ratingText += rating.Source + ':' + rating.Value + "\n"
+                    }
+                    return notMetacritic;
+                });
+
+                console.log("Title: " + data.Title + "\nRelease Year: " + data.Year + "\nCountry: "+ ratingText + data.Country + "\nLanguage: " + data.Language + "\nPlot " + data.Plot + "\nActors: "+ data.Actors);
+
+            }
+        });
+    }
+    if(response.userChoice === "Search Twitter") {
+        client.get('search/tweets', {q: response.searchQuery}, function(error, tweers, response) {
+            var statuses = tweets.statuses.map(function(tweet){return tweet.text});
+            console.log(statuses);
+
+        });
+
+    }
+    if(response.userChoice === "Get song info" {
+        spotify
+        .search({ type: 'track', query: response.searchQuery })
+        .then(function(resp) {
+
+            for (let i = 0; i < resp.tracks.items.lenngth; i++) {
+                let track = resp.tracks.items[i].name
+                let songUrl =resp.tracks.items[i].href
+                let albumName = resp.tracks.items[i].album.name
+                let artists = []
+                for(let j = 0; j < resp.tracks.items[i].artist.length; j++) {
+                    let artistName = resp.tracks.items[i].artists[j].name
+                    artists.push(artistName)
+
+                }
+                console.log(`${i + 1}
+        ${artists.join(",")}
+        ${track}
+        ${songUrl}
+        ${albumName}
+        `)
+            
+                }
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+    }
+});
